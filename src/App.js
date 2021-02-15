2190f538d2e83b31aed20844d488b021b616f4cb
Muhammad Abdullah <kb337137@gmail.com>
2021-02-16 03:02:45 +0500

    still some working example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from "react";
+import pic_bulboff from './pic_bulboff.gif';
+import pic_bulbon from './pic_bulbon.gif';
+import "./index.css";
 
-function App() {
+
+function App({ text, maxlength }) {
+
+  const [hidden, sethidden] = useState(true);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <span>
+        {hidden ? `${text.substr(0, maxlength).trim()} ...` : text}
+        {hidden ? (
+          <a href onClick={() => sethidden(false)}>read more</a>
+        ) : (
+            <a href onClick={() => sethidden(true)}>read less</a>
+          )}
+      </span>
+    </>
   );
 }
+function Check() {
 
-export default App;
+  // const [lights, setlight] = useState(0);
+
+  // function light() {
+  //   setlight(prverious =>
+  //     prverious ? 0 : 1
+  //   )
+  // }
+
+  return (
+    <h1> checl</h1>
+  )
+
+  // return (
+  //   <br>
+  //     <h1 >Second Component</h1>
+  //     <br />
+  //     <img src={pic_bulboff} id={"imgid"} alt="" />
+  //     <br />
+  //     <button type={"button"} onClick={light(1)}>ON</button>
+  //     <br />
+  //     <button type={"button"} onClick={light(0)}> OFF</button>
+
+  //   </br>
+  // )
+}
+
+// function Room() {
+//   const [isLit, setLit] = useState(true);
+
+//   return (
+//     <>
+//       <div className="room">
+//         the room is {isLit ? "lit" : "dark"}
+//         <br />
+//         <button onClick={() => setLit(!isLit)}>
+//           flip
+//         </button>
+//       </div>
+//     </>
+//   )
+// }
+
+function Number() {
+
+  const [number, setnumber] = useState(0);
+
+  return (
+    <>
+      <br />
+      <h1>Your Number Is {number}</h1>
+      <br />
+      <button onClick={() => setnumber(number + 1)}>CLick</button>
+    </>
+  )
+}
+
+export {
+  App, Check, Number
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { App, Check, Number } from "./App";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <App text={
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac scelerisque ligula. Aliquam tempus in tortor sed venenatis. Quisque eget lacinia ante, vel congue nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis et risus ut enim faucibus sollicitudin porta et leo. Maecenas et eros sed nibh volutpat sodales. Vivamus at mauris eget metus semper facilisis eu id diam. Quisque mauris enim, faucibus et tempus quis, dapibus ac dolor. Sed non condimentum erat, quis pulvinar lorem. Donec eleifend luctus mauris ut consequat.`
+    } maxlength={35} />
+    <Check />
+    <Number />
+  </>,
   document.getElementById('root')
 );
 
